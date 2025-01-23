abb7bce8d462520ade24a33f3b99a2adc2b0846a
EwanBailey <ewan.bailey@gmail.com>
2025-01-23 09:07:25 -0800

    Create IfElseTryExcept.py

---
+++ IfElseTryExcept.py
@@ -0,0 +1,16 @@
+while True:
+    
+    user_input = input("Enter a number:")
+    
+    try:
+        x = float(user_input)
+        
+        if(x>5):
+            print (x,"is greater than 5")
+        elif(x<5):
+            print (x,"is less than 5")
+        elif(x==5):
+            print (x,"is equal to 5")
+            
+    except:
+        print ("x is not a number")
