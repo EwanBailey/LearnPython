68fe37ac573ccdbb3a463f498949f8e67dceebe2
EwanBailey <ewan.bailey@gmail.com>
2025-01-23 07:55:34 -0800

    Create Naming.py

---
+++ Naming.py
@@ -0,0 +1,11 @@
+my_var = 6
+YOURvar = 7
+print (YOURvar + my_var)
+  # variable names are case-sensitive
+  # Legal names for variables: 
+    # myvar = "John"
+    # my_var = "John"
+    # _my_var = "John"
+    # myVar = "John"
+    # MYVAR = "John"
+    # myvar2 = "John"
