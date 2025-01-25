5637fb3b068f61bd34631e450354550dc8038e08
EwanBailey <ewan.bailey@gmail.com>
2025-01-24 18:11:02 -0800

    Update and rename GuessNumb.py to GuessRandNumb.py

--- GuessRandNumb.py
+++ GuessRandNumb.py
@@ -1,14 +1,17 @@
+import random
+# Random library is needed for this code
 while True:
     print("Welcome to the game!")
     user_input = input("Guess the number:")
     
     try:
         x = float(user_input)
-        
-        if(x!=5):
+        y = (random.randint(0, 5))
+        # Generates the two numbers
+        if(x!=y):
             print (x,"is not the number...")
-        elif(x==5):
+        elif(x==y):
             print (x,"is the number!!!")
-            
+        # Compares the numbers and displays if it is correct
     except:
         print ("that is not a valid guess")
