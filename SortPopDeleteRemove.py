07fe3f89e9f99e7eb170f47711cccdb12525f18c
EwanBailey <ewan.bailey@gmail.com>
2025-01-30 08:58:22 -0800

    Create SortPopDeleteRemove.py

---
+++ SortPopDeleteRemove.py
@@ -0,0 +1,18 @@
+my_list = ["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d",]
+my_list.sort()
+print(my_list)
+
+my_list2 = ["STEAM", "Clown", "Productions"]
+popped = my_list2.pop(1)
+print(my_list2)
+print(popped)
+
+my_list3 = ["apple", "orange", "pomegranate", "pineapple"]
+print(my_list3)
+del my_list3[1]
+print(my_list3)
+
+# remove() "Shadow"
+my_list4 = ["Centauri", "Minbari", "Narn", "Human", "Shadow", "Vorlon"]
+my_list4.remove("Shadow")
+print(my_list4)
