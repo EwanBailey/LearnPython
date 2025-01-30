bbfd83f47d356b2c9b3936e7724a161c26c93476
EwanBailey <ewan.bailey@gmail.com>
2025-01-30 09:48:03 -0800

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,6 +1,8 @@
 fibonacci = [0, 1]
+y = input("How many digits should be calculated?")
+variable = int(y)
 
-for i in range(10):
+for i in range(variable):
     x = fibonacci[-1] + fibonacci[-2]
     fibonacci.append(x)
 print(fibonacci)
