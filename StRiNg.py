a9a03481f6d10f95331ac84acbaf494c9c53817e
EwanBailey <ewan.bailey@gmail.com>
2025-02-11 08:37:19 -0800

    Create StRiNg.py

---
+++ StRiNg.py
@@ -0,0 +1,6 @@
+my_string = str(input("Please enter a string"))
+print('The variable "my_string" = ',my_string)
+biggest = max(my_string)
+print('The biggest character in the \nstring "STEAM Is Cool" is',biggest)
+smallest = min(my_string)
+print('The smallest character in the \nstring "STEAM Is Cool" is',smallest)
