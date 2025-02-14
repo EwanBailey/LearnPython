a62cc3e1078e1be901e5f7b109f2efbd439b5038
EwanBailey <ewan.bailey@gmail.com>
2025-02-14 08:26:38 -0800

    Create ListVsDict.py

---
+++ ListVsDict.py
@@ -0,0 +1,18 @@
+lst = list()
+lst.append(183)
+lst.append("2:00 PM")
+print(lst)
+lst[1] = "3:00 PM"
+print(lst)
+print(lst[1])
+
+ddd = dict()
+ddd['course'] = 182
+ddd['time'] = "2:00 PM"
+print(ddd)
+ddd['time'] = "3:00 PM"
+print(ddd)
+print(ddd['time'])
+
+print(lst)
+print(ddd)
